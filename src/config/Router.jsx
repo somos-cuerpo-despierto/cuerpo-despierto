81affa7dbef13627f3f048d15edae8390a36f5f9
Israel Espin <israel.espin92@gmail.com>
2025-05-18 19:13:17 +0200

    Initial configuration of the class page by users

--- src/config/Router.jsx
+++ src/config/Router.jsx
@@ -4,24 +4,18 @@ import QuienesSomos from "../pages/QuienesSomos";
 import QueHacemos from'../pages/QueHacemos';
 import Contacto from '../pages/Contacto';
 import IniciarSesion from '../pages/IniciarSesion';
-import { AuthProvider } from './AuthContext';
-
-
-
 
 export default function Router() {
     return (
-        <AuthProvider>
         <BrowserRouter>
-        <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/quienesSomos" element={<QuienesSomos />} />
-            <Route path="/queHacemos" element={<QueHacemos />} />
-            <Route path="/contacto" element={<Contacto />} />
-            <Route path="/iniciarSesion" element={<IniciarSesion /> } />
-        </Routes>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/quienesSomos" element={<QuienesSomos />} />
+                <Route path="/queHacemos" element={<QueHacemos />} />
+                <Route path="/contacto" element={<Contacto />} />
+                <Route path="/iniciarSesion" element={<IniciarSesion />} />
+            </Routes>
         </BrowserRouter>
-        </AuthProvider>
     );
-    }
+}
 
