c48b93e71df62deeb3d0439d31fec4cec1ad3dc6
Israel Espin <israel.espin92@gmail.com>
2025-05-18 10:28:03 +0200

    basic page home star, text welcome and video, navbar

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import './index.css'
-import Router from '../src/config/router/Router.jsx'
+import Router from './config/Router';
 
 
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
     <Router />
-  </StrictMode>,
-)
+  </React.StrictMode>
+);
---
+++ src/pages/Home.jsx
@@ -0,0 +1,59 @@
+import Navbar from "../components/Navbar"
+import { Box, Grid, Typography } from "@mui/material";
+
+const Home = () => {
+  return (
+    <>
+      <div className="home">
+        <Navbar />
+        <Box
+          sx={{
+            flexGrow: 1,
+            minHeight: "80vh",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            mt: 8,
+          }}
+        >
+          <Grid
+            container
+            spacing={4}
+            alignItems="center" // Centra verticalmente los items
+            justifyContent="center" // Centra horizontalmente los items
+          >
+            <Grid item xs={12} md={5} sx={{ display: "flex", justifyContent: "" }}>
+              <Box sx={{ width: "100%", maxWidth: 250 }}>
+                <video
+                  src="src/assets/Resumen Taller.mov"
+                  controls
+                  controlsList="nodownload"
+                  onContextMenu={(e) => e.preventDefault()}
+                  style={{ width: "100%", borderRadius: 16 }}
+                />
+              </Box>
+            </Grid>
+            <Grid item xs={12} md={7} sx={{ display: "flex", flexDirection: "column", justifyContent: "center" }}>
+              <Typography
+                variant="h4"
+                sx={{ fontFamily: "Anton, sans-serif", mb: 2 }}
+              >
+                Bienvenida/o a Cuerpo Despierto
+              </Typography>
+              <Typography variant="body1" sx={{ fontSize: 20 }}>
+                Somos un espacio vivo donde el movimiento se transforma en
+                consciencia, y la fuerza nace desde el respeto al cuerpo.
+                <br />
+                <br />
+                En nuestros talleres despertamos el cuerpo, reconectamos con su inteligencia y lo preparamos para una vida activa, fluida y sin dolor.
+                Próximamente abrimos nuestras clases regulares. ¿Te animas a despertar tu cuerpo con nosotras?
+              </Typography>
+            </Grid>
+          </Grid>
+        </Box>
+      </div>
+    </>
+  );
+};
+
+export default Home;
