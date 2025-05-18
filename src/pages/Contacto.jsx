1e258bcc27d3fbe2c40d6a003e336541f76048ed
Israel Espin <israel.espin92@gmail.com>
2025-05-18 18:14:54 +0200

    adding more width to the contact box

--- src/pages/Contacto.jsx
+++ src/pages/Contacto.jsx
@@ -1,7 +1,7 @@
 import Navbar from "../components/Navbar";
 import Form from "../components/Form";
 import Footer from "../components/Footer";
-import { Box } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 
 const Contacto = () => {
     return (
@@ -14,8 +14,21 @@ const Contacto = () => {
             }}
         >
             <Navbar />
-            <Box sx={{ flex: 1 }}>
-                <Form />
+            <Box sx={{ flex: 1, display: "flex", justifyContent: "center", alignItems: "center" }}>
+                <Box sx={{ width: "100%", maxWidth: 500 }}>
+                    <Typography
+                        variant="h4"
+                        sx={{
+                            fontFamily: "Anton, sans-serif",
+                            mb: 2,
+                            textAlign: "center",
+                            color: "#000",
+                        }}
+                    >
+                        CONTACTO
+                    </Typography>
+                    <Form btnText="ENVIAR" />
+                </Box>
             </Box>
             <Footer />
         </Box>
