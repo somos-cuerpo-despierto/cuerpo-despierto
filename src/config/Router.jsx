a70123a292afac8372636bef3536447b6b95e470
Israel Espin <israel.espin92@gmail.com>
2025-05-18 09:41:16 +0200

    configuration of routes for page navigation

---
+++ src/config/Router.jsx
@@ -0,0 +1,19 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from '../../pages/home/Home';
+
+
+
+export default function Router() {
+    return (
+        <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/quienesSomos" element={<div>¿Quiénes Somos?</div>} />
+            <Route path="/queHacemos" element={<div>¿Qué Hacemos?</div>} />
+            <Route path="/contacto" element={<div>Contacto</div>} />
+            <Route path="/iniciarSesion" element={<div>Iniciar Sesión</div>} />
+        </Routes>
+        </BrowserRouter>
+    );
+    }
+
