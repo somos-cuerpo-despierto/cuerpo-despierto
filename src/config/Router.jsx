a9b13b8bd8f401e8988620b586a162c81132b93f
Israel Espin <israel.espin92@gmail.com>
2025-05-18 14:07:14 +0200

    component formulario and page contacto

--- src/config/Router.jsx
+++ src/config/Router.jsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from '../pages/Home';
 import QuienesSomos from "../pages/QuienesSomos";
+import QueHacemos from'../pages/QueHacemos';
+import Contacto from '../pages/Contacto';
 
 
 
@@ -10,8 +12,8 @@ export default function Router() {
         <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/quienesSomos" element={<QuienesSomos />} />
-            <Route path="/queHacemos" element={<div>¿Qué Hacemos?</div>} />
-            <Route path="/contacto" element={<div>Contacto</div>} />
+            <Route path="/queHacemos" element={<QueHacemos />} />
+            <Route path="/contacto" element={<Contacto />} />
             <Route path="/iniciarSesion" element={<div>Iniciar Sesión</div>} />
         </Routes>
         </BrowserRouter>
---
+++ src/pages/Contacto.jsx
@@ -0,0 +1,15 @@
+import Footer from "../components/Footer";
+import FormularioContacto from "../components/FormularioContacto";
+
+const Contacto = () => {
+
+    return (       
+         <>
+            <Navbar />
+            <FormularioContacto />
+            <Footer />
+        </>
+    );
+}
+
+export default Contacto;
